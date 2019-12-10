4cd47e84f6c1c1cac106e30e2433ef8c74ff4f52
Akash.Mazumdar <Akash.Mazumdar@LAP-TZ-0X67990.IN.NIIT-Tech.com>
2019-12-10 21:22:23 +0530

    Second commit

--- src/main/java/com/stackSimplify/restServices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stackSimplify/restServices/SpringbootBuildingblocksApplication.java
@@ -3,6 +3,8 @@ package com.stackSimplify.restServices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//SpringBoot Application 
+
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
